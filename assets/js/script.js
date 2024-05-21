5a0dcaa9e91ee78bcf8301cbe16730f7abd8b382
lluiziull <luizkirsch0@gmail.com>
2024-05-20 23:11:48 -0300

    feat: add discord webhook

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,3 +152,57 @@ function enviarWhatsApp() {
     }
 }
 
+function enviarParaDiscord() {
+    var mensagem = document.getElementById("horarioInput").value;
+    var numeroWhatsApp = "5551995672942";
+    var checkboxes = document.getElementsByName("restaurant");
+    var restauranteSelecionado = "";
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            restauranteSelecionado = checkboxes[i].value;
+            break;
+        }
+    }
+
+    if (mensagem.trim() !== "" && restauranteSelecionado.trim() !== "") {
+        var textoMensagem = "";
+        if (restauranteSelecionado === "@here Me surpreenda") {
+            textoMensagem = restauranteSelecionado + " às " + mensagem;
+        } else if (restauranteSelecionado === "Eu vou te surpreender") {
+            textoMensagem = "@here Me busca às " + mensagem + " que eu vou te levar em algum lugar.";
+        } else if (restauranteSelecionado === "sugestao") {
+            var sugestao = document.getElementById("texterea").value;
+            textoMensagem = "@here Pô bora no " + sugestao + " às " + mensagem;
+        } else {
+            textoMensagem = "@here Bora no " + restauranteSelecionado + " às " + mensagem;
+        }
+
+        var data = {
+            content: textoMensagem
+        };
+
+        fetch("https://discord.com/api/webhooks/1242296272774500382/-9T5OPLt2OoZvGZtT_1de9m6fnvbnu5od9cfe2Qy4NqqaqnK4geU9gUn2_6O4MibuYaG", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao enviar mensagem para o Discord');
+            }
+            console.log('Mensagem enviada com sucesso para o Discord');
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+
+        document.getElementById("horarioInput").value = "";
+        checkboxes.forEach((checkbox) => {
+            checkbox.checked = false;
+        });
+        checkConditions();
+    }
+}
